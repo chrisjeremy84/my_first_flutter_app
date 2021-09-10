172eb0d763c0343934f8d7beda769cae1e13b47c
5thfader <chrisjeremy84@live.com>
2021-09-10 14:56:34 +0200

    Widgets, Images, Fonts and assets done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text('Chris first Flutter App'),
-  ));
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chris's APP"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[300],
+      ),
+      body: Center(
+          child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/4553619/pexels-photo-4553619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+      )),
+      backgroundColor: Colors.red[200],
+    );
+  }
 }
