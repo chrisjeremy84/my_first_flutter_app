a98904f571fb31f5c76bd2130da7e5604a546efa
5thfader <chrisjeremy84@live.com>
2021-09-24 08:49:47 +0200

    ID card created

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,79 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
-        title: Text("Chris's APP"),
+        title: Text("ID APP"),
         centerTitle: true,
-        backgroundColor: Colors.blue[300],
+        backgroundColor: Colors.grey[600],
+        elevation: 0.0,
       ),
-      body: Center(
-          child: IconButton(
-        icon: Icon(Icons.chat_bubble_outline_rounded),
-        onPressed: () {
-          print("You clicked the button");
-        },
-        iconSize: 35,
-      )),
-      backgroundColor: Colors.red[200],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 30, 30, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/Matt.jpg'),
+                radius: 70.0,
+              ),
+            ),
+            Divider(
+              height: 40,
+              color: Colors.cyan,
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Matt Benson',
+              style: TextStyle(
+                  color: Colors.cyan[300],
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Followers',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '16',
+              style: TextStyle(
+                  color: Colors.cyan[300],
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.grey[350],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  '736 871 771',
+                  style: TextStyle(
+                      color: Colors.cyan[300],
+                      fontSize: 20,
+                      letterSpacing: 1.0,
+                      fontWeight: FontWeight.w700),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
